bf3ebee7dc330b7a6a765e7c689a3b1e7c987bde
alisonsouzalt <you@example.com>
2024-11-07 21:09:33 -0300

    att

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,18 @@
+using System.Dynamic;
+
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+       public Nokia(string numero, string modelo, string imei, int memoria)
+            : base(numero, modelo, imei, memoria) { }
+
+        // Implementação específica de InstalarAplicativo para Nokia
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no Nokia.");
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia("123456789", "Nokia 3310", "1111111111", 64);
+nokia.InstalarAplicativo("WhatsApp");
+
+Smartphone iphone = new Iphone("987654321", "iPhone 14", "2222222222", 128);
+iphone.InstalarAplicativo("Instagram");
